ba24b87fbccc5fe5f158a38b94f6f9674addef51
Hector Flores Rodriguez <hfloresr@uci.edu>
2018-05-05 23:09:31 -0700

    updated code for more/better data structures

--- mysub.c
+++ mysub.c
@@ -1,11 +1,126 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define INITCAP 32
 
 
 typedef struct {
-    int result;
-    int ind[4];
     int size;
-} qbucket;
+    int capacity;
+    int *data;
+} Set;
+
+void init_set(Set *set)
+{
+    set->size = 0;
+    set->capacity = INITCAP;
+    set->data = malloc(sizeof(int) * set->capacity);
+}
+
+void grow_set(Set *set)
+{
+    set->capacity *= 2;
+    set->data = realloc(set->data, sizeof(int) * set->capacity);
+}
+
+void add_data(Set *set, int value)
+{
+    if (set->size >= set->capacity)
+        grow_set(set);
+
+    set->data[set->size++] = value;
+}
+
+int get_data(Set *set, int idx)
+{
+    if (idx < set->size || idx >= 0)
+        return set->data[idx];
+
+    printf("Index %d out of bounds\n", idx);
+    return -88;
+}
+
+void array_to_set(Set *set, int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; ++i)
+        add_data(set, arr[i]);
+}
+
+
+void join_sets(Set *a, Set *b)
+{
+    int i, x;
+    for (i = 0; i < b->size; ++i) {
+        x = get_data(b, i);
+        add_data(a, x);
+    }
+}
+
+
+void free_set(Set *set)
+{
+    free(set->data);
+    set->size = set->capacity = 0;
+}
+
+
+typedef struct {
+    int size;
+    Set list[512];
+} Bucket;
+
+void init_bucket_list(Bucket *b)
+{
+    int i;
+    for (i = 0; i < 512; ++i)
+        init_set(&b->list[i]);
+    b->size = 0;
+}
+
+void clear_buckets(Bucket *b)
+{
+    int i;
+    for (i = 511; i >= 0 ; --i)
+        free_set(&b->list[i]);
+    b->size = 0;
+}
+
+int add_array_to_bucket(Bucket *b, int arr[], int n)
+{
+    int is_full;
+
+    if ((is_full = b->size >= 512) == 1)
+        printf("Bucket is full\n");
+    else
+        array_to_set(&b->list[b->size++], arr, n);
+    return (is_full ? 0 : 1);
+}
+
+
+int add_set_to_bucket(Bucket *b, Set *s)
+{
+    int is_full;
+
+    if ((is_full = b->size >= 512) == 1)
+        printf("Bucket is full\n");
+    else
+        b->list[b->size++] = *s;
+
+
+    return (is_full ? 0 : 1);
+}
+
+
+void remove_from_bucket(Bucket *b, int idx)
+{
+    if(idx < b->size && idx >= 0)
+        b->list[idx] = b->list[b->size--];
+
+    b->size--;
+}
+
+
 
 
 /* Boyer-Moor majority vote algorithm */
@@ -36,23 +151,6 @@ void init_indices(int arr[], int n)
         arr[i] = i+1;
 }
 
-void init_buckets(qbucket arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n; ++i) {
-            arr[i].result = -13;
-            for (j = 0; j < 4; ++j)
-                arr[i].ind[j]= 0;
-    }
-}
-
-void update_indices(qbucket b[], int query[], int cnt)
-{
-    int i;
-    for (i = 0; i < 4; ++i)
-        b[cnt].ind[i]  = query[i];
-}
-
 
 void new_query(int query[], int indices[], int sz, int start)
 {
@@ -62,87 +160,52 @@ void new_query(int query[], int indices[], int sz, int start)
     }
 }
 
-void combine_b4_indices(qbucket b[], int query[], int bin1, int bin2)
+int map_qcount(int qresult)
 {
-    query[0] = qbucket[bin1].ind[0];
-    query[1] = qbucket[bin1].ind[1];
-    query[2] = qbucket[bin2].ind[2];
-    query[3] = qbucket[bin2].ind[3];
-}
-
-
-/* bin1 < bin 2 */
-void remove_bin1_bin2(qbucket, b[], int sz, int bin1, int bin2)
-{
-    if (bin2 < sz-2)
-        qbucket[bin2] = qbucket[sz-2];
-    if (bin1 < sz-2)
-        qbucket[bin1] = qbucket[sz-1];
-}
-
-void print_bucket(qbucket b[], int sz)
-{
-    int i, j;
-    for (i = 0; i < sz; ++i) {
-        printf("set: %d\n", i);
-        printf("\tindices: ");
-        for (j = 0; j < 4; ++j) {
-            printf("%d, ", b[i].ind[j]);
-        }
-        printf("\n");
-    }
-
+    return qresult == 0 ? 0 :
+           qresult == 2 ? 1 :
+           qresult == 4 ? 2 :
+           qresult;
 }
 
 int mysub(int n)
 {
-    int i, j;
-    int qsize, qresult, bsize;
-
-    qsize = 4;                  /* query size is 4 */
-    bsize = (n / 2) + 1;        /* bin size */
+    int i;
+    int qsize, qresult, qbin;
 
     /* indices of marbles */
     int indices[n];
     init_indices(indices, n);
 
+    qsize = 4;                  /* query size is 4 */
     int query[qsize];           /* store current query */
-    qbucket b4[bsize];          /* store queries that resulted in 4 */
-    qbucket b2[bsize];          /* store queries that resulted in 2 */
-    qbucket b0[bsize];          /* store queries that resulted in 0 (equal 1's and 0's )*/
 
-    init_buckets(b4, bsize);
-    init_buckets(b2, bsize);
-    init_buckets(b0, bsize);
-
-    int cnt[3] = {0, 0, 0};
+    /* qresult=0 -> B[0] ; qresult=2 -> B[1] ; qresult=4 -> B[2] */
+    Bucket B[3];
+    for (i = 0; i < 3; ++i)
+        init_bucket_list(&B[i]);
 
     for (i = 0; i < n; i+=4) {
         new_query(query, indices, qsize, i);
         qresult = QCOUNT(1, query);
-        if (qresult == 4) {
-            update_indices(b4, query, cnt[2]);
-            cnt[2]++;
-        }
-        else if (qresult == 2) {
-            update_indices(b2, query, cnt[1]);
-            cnt[1]++;
-        }
-        else if (qresult == 0) {
-            update_indices(b0, query, cnt[0]);
-            cnt[0]++;
-        }
+        qbin = map_qcount(qresult);
+        printf("(qresult, qbin) = (%d, %d)\n", qresult, qbin);
+        if (qbin >= 0)
+            add_array_to_bucket(&B[qbin], query, qsize);
         else
             break;
     }
 
-    qbucket set[cnt[2]];
+    for (i = 0; i < 3; ++i) {
+        printf("B[%d] size = %d\n", i, B[i].size);
+    }
 
-    j = 0;
+
+    /*
     i = 0;
-    for (j = 1; j < cnt[2]; ++j)
+    int bin1, bin2;
     while (i < cnt[2]) {
-        bin1 = i, bin2 = bin1+j;
+        bin1 = i, bin2 = bin1+1;
         combine_b4_indices(b4, query, bin1, bin2);
         qresult = QCOUNT(1, query);
         if (qresult == 0) {
@@ -155,17 +218,10 @@ int mysub(int n)
             i += 2;
         }
     }
+    */
 
-    printf("cnt4 = %d\tcnt2 = %d\tctn0 = %d\n", cnt[2], cnt[1], cnt[0]);
-
-    printf("\nbucket 4:\n");
-    print_bucket(b4, cnt[2]);
-
-    printf("\nbucket 2:\n");
-    print_bucket(b2, cnt[1]);
-
-    printf("\nbucket 0:\n");
-    print_bucket(b0, cnt[0]);
+    for (i = 2; i >= 0 ; --i)
+        clear_buckets(&B[i]);
 
     return 1;
 }
